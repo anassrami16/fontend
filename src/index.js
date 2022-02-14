d56fb66134a0f69eb45c0f6cfafd2b60f2f61bf1
YassineElmarraki <74514663+YassinMarraki@users.noreply.github.com>
2022-02-14 06:02:17 +0100

    add admin space

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./App";
 
-import App from './App';
+import Accueil from "./Accueil";
+import Presentation from "./Presentation";
+import Connecter from "./Connecter";
+import Inscription from "./Inscription";
+import Admin from "./Admin";
 
-import Accueil from './Accueil';
-import Presentation from './Presentation';
-import Connecter from './Connecter';
-import Inscription from './Inscription';
-
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-          <Routes>
-            <Route path="/presentation" element={<Presentation />} />
-            <Route path="/inscription" element={<Inscription />} />
-            <Route path="/connecter" element={<Connecter />} />
-            <Route path="/decouvrir" element={<App />} />
-            <Route path="/" element={<Accueil />} />
-          </Routes>
+      <Routes>
+        <Route path="/presentation" element={<Presentation />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/connecter" element={<Connecter />} />
+        <Route path="/decouvrir" element={<App />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<Accueil />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-
-
-
-
-
-
